50709ba4581123bfabbbb791cc0629e3aa554ff7
RonanW123 <142534416+RonanW123@users.noreply.github.com>
2024-04-22 20:34:33 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -2,4 +2,25 @@ import java.util.ArrayList;
 public class StepTracker
 {
  /* to be implemented here */
+ private ArrayList <int> a;
+ private int b, c, d;
+ public StepTracker(int mSteps){
+  a = new ArrayList<Double>();
+  b = 0;
+  c = mSteps;
+  d = 0;
+ }
+ public void addDailySteps(int dSteps){
+  a.add(dSteps);
+  if(dSteps >= c)
+   b++;
+ }
+ public int activeDays(){
+  return b;
+ }
+ public double averageSteps(){
+  for(int i = 0; i < a.size(); i++)
+    d += a.get(i);
+  return (double) d/a.size();
+ }
 } 
